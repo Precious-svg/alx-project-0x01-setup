c4a9b8a39c5a0eaf2539d2b84f58ce960f3303b9
Precious Ogwezhi <pogwezhi@gmail.com>
2025-05-30 17:48:26 +0100

    map

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -30,7 +30,7 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
       </div>
 
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {users.map((user) => (
+        {posts.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
       </div>
