7a929a5faae747a5d9be00548fe88f9539eeb763
Precious Ogwezhi <pogwezhi@gmail.com>
2025-05-30 17:44:49 +0100

    button

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,9 @@
+const Button : React.FC = () => {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <button className="text-7xl font-thin">Button Page</button>
+      </div>
+    )
+  }
+  
+   export default Button;
---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <footer>
+            <h1 className="text-7xl font-thin">Welcome Page</h1>
+        </footer>
+      </div>
+    )
+  }
+  
+   export default Footer;
